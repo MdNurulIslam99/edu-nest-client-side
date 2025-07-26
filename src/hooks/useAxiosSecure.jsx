3e2271772a059dc3831fc3a006caacf5f55feb10
MdNurulIslam99 <mdnurulislam.eng@gmail.com>
2025-07-26 12:12:39 +0600

    update to localhost to vercel link

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,8 +2,9 @@ import axios from "axios";
 import React from "react";
 
 const axiosSecure = axios.create({
-  baseURL: `http://localhost:5000`,
+  baseURL: `https://edunest-server-side.vercel.app`,
   // https://edunest-server-side.vercel.app
+  // http://localhost:5000
 });
 
 const useAxiosSecure = () => {
