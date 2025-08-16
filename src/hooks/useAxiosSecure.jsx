b2aeb5dca6d5d20afabaef4ca745c64d6bef345a
MdNurulIslam99 <mdnurulislam.eng@gmail.com>
2025-08-17 00:56:36 +0600

    updated ripo

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,13 +1,24 @@
 import axios from "axios";
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../Context/AuthContext/AuthContext";
 
 const axiosSecure = axios.create({
-  baseURL: `https://edunest-server-side.vercel.app`,
+  baseURL: `http://localhost:5000`,
   // https://edunest-server-side.vercel.app
   // http://localhost:5000
 });
 
 const useAxiosSecure = () => {
+  const { user } = useContext(AuthContext);
+  axiosSecure.interceptors.request.use(
+    (config) => {
+      config.headers.Authorization = `Bearer ${user.accessToken}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
   return axiosSecure;
 };
 
